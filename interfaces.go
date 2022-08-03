1a4a61d5d396d44b418772c877a64fcc96d853c3
AlexeyYakovlev1 <alexeyykovlev555@gmail.com>
2022-08-03 22:40:27 +0300

    two lesson init

---
+++ interfaces.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Shape interface {
+	Area() float32
+}
+
+type Square struct {
+	sideLength float32
+}
+
+func (s Square) Area() float32 {
+	return s.sideLength * s.sideLength
+}
+
+type Circle struct {
+	radius float32
+}
+
+func (c Circle) Area() float32 {
+	return float32(math.Pi) * (c.radius * c.radius)
+}
+
+func main() {
+	square := Square{10.5}
+	circle := Circle{10.3}
+
+	printShapeArea(square)
+	printShapeArea(circle)
+}
+
+func printShapeArea(s Shape) {
+	fmt.Printf("Area shape: %.2f sm\n", s.Area())
+}
